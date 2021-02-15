25c557603026f4042ff377a2a1e7a57616159245
lxh6688 <1272662790@qq.comom>
2021-02-15 11:37:14 +0800

    获取 input 输入值

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,10 +2,17 @@ import React, { Component } from 'react'
 import './index.css'
 
 export default class Header extends Component {
+
+  handleKeyUp = (event) => {
+    const {keyCode,target} = event
+    if(keyCode !== 13) return
+    console.log(target.value)
+  }
+
   render() {
     return (
       <div className="todo-header">
-        <input type="text" placeholder="请输入你的任务名称，按回车键确认" />
+        <input onKeyUp={this.handleKeyUp} type="text" placeholder="请输入你的任务名称，按回车键确认" />
       </div>
     )
   }
