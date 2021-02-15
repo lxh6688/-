54f09423be0d82eb7be291093e99cab840c32053
lxh6688 <1272662790@qq.comom>
2021-02-15 17:08:34 +0800

    完成Footer组件

--- src/App.jsx
+++ src/App.jsx
@@ -40,6 +40,22 @@ export default class App extends Component {
     this.setState({todos:newTodos})
   }
 
+  checkAllTodo = (done) => {
+    const {todos} = this.state
+    const newTodos = todos.map((todoObj)=> {
+      return {...todoObj,done}
+    })
+    this.setState({todos:newTodos})
+  }
+
+  clearAllDown = (done)=> {
+    const {todos} = this.state
+    const newTodos = todos.filter((todoObj) => {
+      return !todoObj.done
+    })
+    this.setState({todos:newTodos})
+  }
+
   render() {
     const {todos} = this.state
     return (
@@ -47,7 +63,7 @@ export default class App extends Component {
         <div className="todo-wrap">
           <Header addTodo={this.addTodo}/>
           <List todos={todos} updateTodo={this.updateTodo} deleteTodo={this.deleteTodo}/>
-          <Footer/>
+          <Footer todos={todos} checkAllTodo={this.checkAllTodo} clearAllDown={this.clearAllDown}/>
         </div>
       </div>
     )
